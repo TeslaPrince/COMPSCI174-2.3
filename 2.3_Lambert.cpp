982279aa878f966419d3dc45571c5efa90674b1e
Tesla Prince <57016635+TeslaPrince@users.noreply.github.com>
2021-01-21 00:37:02 -0600

    Add files via upload

---
+++ 2.3_Lambert.cpp
@@ -0,0 +1,17 @@
+//The Works of Wolfgang
+#include <iostream>
+using namespace std;
+int main();
+{
+cout<<"The works of Wolfgang\ninclude the following";
+cout<<"\nThe Turkish March"<<endl;
+cout<<"and Symphony No. 40";
+cout<<"in G minor."<<endl;
+return 0;
+}
+//output
+//The works of Wolfgang
+//include the following
+//
+//The Turkish March
+//and Symphony No. 40 in G minor.
